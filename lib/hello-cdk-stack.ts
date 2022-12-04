9ec4175e1008a1b49963c7193768534114a0c085
mahmoudokily <mahmoudokily93@gmail.com>
2022-12-04 15:57:32 +0100

    add s3 bucket config

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -7,10 +7,27 @@ import {
   CodePipelineSource,
   ShellStep,
 } from "aws-cdk-lib/pipelines";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // create s3 bucket to store the website files
+    const bucket = new s3.Bucket(this, "WebsiteBucket", {
+      bucketName: "techit-website",
+      websiteIndexDocument: "index.html",
+      blockPublicAccess: new s3.BlockPublicAccess({
+        restrictPublicBuckets: false,
+      }), // 2
+    });
+    const bucketPolicy = new iam.PolicyStatement({
+      actions: ["s3:GetObject"],
+      resources: [`${bucket.bucketArn}/*`],
+      principals: [new iam.AnyPrincipal()],
+    });
+    bucket.addToResourcePolicy(bucketPolicy);
 
     const Pipeline = new CodePipeline(this, "Pipeline", {
       pipelineName: "TestTechit",
